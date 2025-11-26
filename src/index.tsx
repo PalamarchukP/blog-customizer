12c81aa23e0b9d75a5283c7b3a19e74220f8ab8d
Polina Palamarchuk <polina.palamarchuk14@gmail.com>
2025-11-27 01:33:35 +0200

    feat: добавлен функционал изменения стилей в проекте

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,31 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+import {
+	ThemeProvider,
+	useTheme,
+} from './components/article-params-form/hooks/useThemeContext';
+import { ArticleParamsForm } from './components/article-params-form';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const { theme } = useTheme();
+	const style: React.CSSProperties = {
+		'--font-family': theme.fontFamilyOption.value,
+		'--font-size': theme.fontSizeOption.value,
+		'--font-color': theme.fontColor.value,
+		'--container-width': theme.contentWidth.value,
+		'--bg-color': theme.backgroundColor.value,
+	} as React.CSSProperties;
 	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
+		<main className={clsx(styles.main)} style={style}>
 			<ArticleParamsForm />
 			<Article />
 		</main>
@@ -33,6 +34,8 @@ const App = () => {
 
 root.render(
 	<StrictMode>
-		<App />
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
 	</StrictMode>
 );
